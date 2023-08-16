127521d39e6b05212f956b9feb78c6ed78188fc7
daffamf <daffamuhammadfirdaus5@gmail.com>
2023-08-16 18:43:16 +0700

    “test“

---
+++ jwt.js
@@ -0,0 +1,31 @@
+const jwToken = `eyJhbGciOiJSUzI1NiIsIng1dCI6IjdkRC1nZWNOZ1gxWmY
+3R0xrT3ZwT0IyZGNWQSIsInR5cCI6IkpXVCJ9.
+eyJhdWQiOiJodHRwczovL2NvbnRvc28uY29tIiw
+iaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQv
+ZTQ4MTc0N2YtNWRhNy00NTM4LWNiYmUtNjdlNTdmN
+2QyMTRlLyIsIm5iZiI6MTM5MTIxMDg1MCwiZXhwIj
+oxMzkxMjE0NDUwLCJzdWIiOiIyMTc0OWRhYWUyYTkx
+MTM3YzI1OTE5MTYyMmZhMSJ9.C4Ny4LeVjEEEybcA1SVaFYFS6nH-Ezae_RrTXUYInjXGt
+-vBOkAa2ryb-kpOlzU_R4Ydce9tKDNp1qZTomXgHjl
+-cKybAz0Ut90-dlWgXGvJYFkWRXJ4J0JyS893EDwTEHYaAZH_
+lCBvoYPhXexD2yt1b-73xSP6oxVlc_sMvz3DY__1Y_OyvbYrThHnHglxvjh88x
+_lX7RN-Bq82ztumxy97rTWaa_1WJgYuy7h7okD24FtsD9PPLYAply0ygl
+31ReI0FZOdX12Hl4THJm4uI_4_bPXL6YR2oZhYWp-4POWIPHzG9c_GL8asBjoDY9F5q
+1ykQiotUBESoMML7_N1g`
+
+
+
+const decodeHasil =` {
+    {
+        alg: "RS256",
+            x5t: "7dD-gecNgX1Zf7GLkOvpOB2dcVA",
+                typ: "JWT"
+    }.
+    {
+        aud: "https://contoso.com",
+            iss: "https://sts.windows.net/e481747f-5da7-4538-cbbe-67e57f7d214e/",
+                nbf: 1391210850,
+                    exp: 1391214450,
+                        sub: "21749daae2a91137c259191622fa1"
+    }
+}`
